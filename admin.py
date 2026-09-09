bf2e25d106177a713424809bd0c0854c74681c62
Avery Hendrix <ahendrix1@luc.edu>
2026-09-09 13:53:38 -0500

    working xml yoink

--- admin.py
+++ admin.py
@@ -5,12 +5,15 @@ import numpy as np
 tree = ET.parse('adminSalary.xml')
 root = tree.getroot()
 
-
-officerList = np.array([],[])
+# stupid. to be made better later
+names = []
+titles = []
 
 for officer in root.iter('{http://www.irs.gov/efile}RltdOrgOfficerTrstKeyEmplGrp'):
-    for field in officer.findall('*'):
-        officerList = officerList.append(field.find('{http://www.irs.gov/efile}PersonNm'))
+    for name in officer.findall('{http://www.irs.gov/efile}PersonNm'):
+        names.append(name.text)
+    for title in officer.findall('{http://www.irs.gov/efile}TitleTxt'):
+        titles.append(title.text)
+officers = np.array([names, titles])
 
-        
-
+print(officers)
