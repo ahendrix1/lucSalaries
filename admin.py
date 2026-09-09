506bbc8343d7bcb0c673972a0b3c4b7209944abd
Avery Hendrix <ahendrix1@luc.edu>
2026-09-09 15:01:24 -0500

    good looking table

--- admin.py
+++ admin.py
@@ -3,24 +3,30 @@ import numpy as np
 import pandas as pd
 
 # parses xml doc into memory as a tree
-tree = ET.parse('adminSalary.xml')
+tree = ET.parse("adminSalary.xml")
 root = tree.getroot()
 
 # stupid. to be made better later
+
 names = []
 titles = []
 salaries = []
 
-for officer in root.iter('{http://www.irs.gov/efile}RltdOrgOfficerTrstKeyEmplGrp'):
-    for name in officer.findall('{http://www.irs.gov/efile}PersonNm'):
+for officer in root.iter(
+    "{http://www.irs.gov/efile}RltdOrgOfficerTrstKeyEmplGrp"
+):
+    for name in officer.findall("{http://www.irs.gov/efile}PersonNm"):
         names.append(name.text)
-    for title in officer.findall('{http://www.irs.gov/efile}TitleTxt'):
+    for title in officer.findall("{http://www.irs.gov/efile}TitleTxt"):
         titles.append(title.text)
-    for salary in officer.findall('{http://www.irs.gov/efile}TotalCompensationFilingOrgAmt'):
+    for salary in officer.findall(
+        "{http://www.irs.gov/efile}TotalCompensationFilingOrgAmt"
+    ):
         salaries.append(salary.text)
 
 officers = np.array([names, titles, salaries])
 
-table = pd.DataFrame(officers)
+table = pd.DataFrame(officers.T)
 
-table.to_csv("admin_out.csv")
+table.to_csv("admin_out.csv", index=False)
+
