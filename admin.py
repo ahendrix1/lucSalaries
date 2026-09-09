1498d99bfbe0caaa66342b0569092d37d4443d64
Avery Hendrix <ahendrix1@luc.edu>
2026-09-09 13:17:12 -0500

    started work!

--- admin.py
+++ admin.py
@@ -1,12 +1,16 @@
 import xml.etree.ElementTree as ET
+import numpy as np
 
 # parses xml doc into memory as a tree
 tree = ET.parse('adminSalary.xml')
 root = tree.getroot()
 
-officerList = []
+
+officerList = np.array([],[])
 
 for officer in root.iter('{http://www.irs.gov/efile}RltdOrgOfficerTrstKeyEmplGrp'):
-    officerList.append(officer)
-print(officerList)
+    for field in officer.findall('*'):
+        officerList = officerList.append(field.find('{http://www.irs.gov/efile}PersonNm'))
+
+        
 
