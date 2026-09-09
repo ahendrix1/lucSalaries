1774d43215a7cf3d70369e6b167ab947a7ef1841
Avery Hendrix <ahendrix1@luc.edu>
2026-09-09 14:18:31 -0500

    bad looking table

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import numpy as np
+import pandas as pd
 
 # parses xml doc into memory as a tree
 tree = ET.parse('adminSalary.xml')
@@ -8,12 +9,18 @@ root = tree.getroot()
 # stupid. to be made better later
 names = []
 titles = []
+salaries = []
 
 for officer in root.iter('{http://www.irs.gov/efile}RltdOrgOfficerTrstKeyEmplGrp'):
     for name in officer.findall('{http://www.irs.gov/efile}PersonNm'):
         names.append(name.text)
     for title in officer.findall('{http://www.irs.gov/efile}TitleTxt'):
         titles.append(title.text)
-officers = np.array([names, titles])
+    for salary in officer.findall('{http://www.irs.gov/efile}TotalCompensationFilingOrgAmt'):
+        salaries.append(salary.text)
 
-print(officers)
+officers = np.array([names, titles, salaries])
+
+table = pd.DataFrame(officers)
+
+table.to_csv("admin_out.csv")
